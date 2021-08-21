98d9432a927951a33f2aae3078bb4b81cd43e9d9
alexbob17 <57636640+alexbob17@users.noreply.github.com>
2021-08-20 21:54:02 -0600

    Primeros archivos

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+/*!
+ * Description: Personal Portfolio Template
+ * Author/Developer: Exill
+ * Author/Developer URL: ALejandro Morales
+ * Version: v1.0.0
+ */
+! function(n) {
+    "use strict";
+    n((function() {})), n(window).on("load", (function() {}))
+}(jQuery);
